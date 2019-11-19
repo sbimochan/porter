65cc9659841bfcb7f294cd2ce576a20e37190fc3
Bimochan Shrestha <bmochan@gmail.com>
2019-11-19 22:32:47 +0545

    read-yaml: Use with yaml and code refactor

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
-import subprocess
-from subprocess import CalledProcessError
+from subprocess import CalledProcessError, check_output
+from os import path
+from yaml import safe_load as yaml_load
 
+EMPTY_SPACES = "    "
 def print_green(result):
-	print("\x1b[32m" + result + "\x1b[0m")
+    print("\x1b[32m" + result + "\x1b[0m")
 
 def print_red(result):
-	print("\x1b[31m" + result + "\x1b[0m")
+    print("\x1b[31m" + result + "\x1b[0m")
 
-def main():
-	try:
-		status = subprocess.check_output("netstat -plunt", shell=True)
-	except CalledProcessError as identifier:
-		print("Please install netstat to run this service.")
-		return
-	check_ports(status, "0.0.0.0:5000   ", "Flask is") # White spaces to make search exact
-	check_ports(status, "0.0.0.0:443    ", "SSL is")
-	check_ports(status, "0.0.0.0:8080   ", "Airflow is")
-	check_ports(status, "0.0.0.0:80     ", "nginx")
-	check_ports(status, "0.0.0.0:5432   ", "Local postgres is")
+def check_service_status(status, port_to_check, message):
+    if port_to_check in status:
+	    print_green(message + " is running")
+    else:
+	    print_red(message + " is not running")
 
-def check_ports(status, port_to_check, message):
-	if port_to_check in status:
-		print_green(message + " running")
-	else:
-		print_red(message + " not running")
+def check_script_status(file_location, message):
+    if path.exists(file_location):
+        print_green(message + " file exists.")
+    else:
+        print_red(message + " file doesn't exist.")
+
+def check_status():
+    config = yaml_load(open("service_lists.yml"))
+    for script, script_location in config["files"].items():
+        check_script_status(script_location, script)
+    try:
+	    status = check_output("netstat -plunt", shell=True)
+    except CalledProcessError as identifier:
+	    print("Please install netstat to run this service.")
+	    return
+
+    for service_name, port in config["services"].items():
+        check_service_status(status, port + EMPTY_SPACES, service_name + " is")
+
 
 if __name__ == "__main__":
-	main()
+    check_status()
